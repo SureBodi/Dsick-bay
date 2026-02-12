8d470444a062862d86deb3d7322b4189f005fe05
SureBodi <info.surebodi@gmail.com>
2026-02-12 12:51:25 +0100

    Update AboutContents.jsx

--- src/components/AboutContents.jsx
+++ src/components/AboutContents.jsx
@@ -56,7 +56,7 @@ const AboutContents = () => {
             <div className="cs_about_img">
               <img
                 // src="assets/img/home_2/about.jpg"
-                src="/assets/img/WhatsApp Image 1404-07-07 at 23.53.34.jpeg"
+                src="/assets/img/about_dsickbay.png"
                 alt="About"
                 className="cs_radius_30 w-100"
               />
